7e488149d07a73e28ad85a46cced420bb774c4bb
10190997 <petrocollege10190997@gmail.com>
2021-10-07 20:05:34 +0300

    помойка помойки помойная помойка

--- EEEEEEEEE/Program.cs
+++ EEEEEEEEE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -9,18 +10,22 @@ namespace EEEEEEEEE
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string url = @"https://portal.petrocollege.ru/Lists/2014/Attachments/10/%D0%B3%D1%80%D1%83%D0%BF%D0%BF_27.09.xlsx";
 
-            using WebClient client = new WebClient
+            string url = @"https://portal.petrocollege.ru/Lists/2014/Attachments/10/%D0%B3%D1%80%D1%83%D0%BF%D0%BF_27.09.xlsx";
+            
+            using WebClient client = new()
             {
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("10190997", "2KU3bmZWyC")
+                Credentials = new NetworkCredential("10190997", "2KU3bmZWyC"),
             };
             client.Headers.Add(HttpRequestHeader.Cookie, null);
-            client.DownloadFile(url, "timetable.xlsx");
+            client.DownloadFile(url, "../../../resources/timetable.xlsx");
+            client.Dispose(); // ВАЖНО!! закрывает процесс экселя. иначе будет конфликт процессов
 
-            Excel.Application ObjWorkExcel = new Excel.Application(); //открыть эксель
-            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(url); //открыть файл
+            string filePath = Path.GetFullPath("../../../resources/timetable.xlsx"); // получает путь к файлу
+
+            Excel.Application ObjWorkExcel = new(); //открыть эксель
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(filePath); //открыть файл 
             Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1 лист
             var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//1 ячейку
             string[,] list = new string[lastCell.Column, lastCell.Row]; // массив значений с листа равен по размеру листу
@@ -34,9 +39,15 @@ namespace EEEEEEEEE
                 }
             }
 
-            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            ObjWorkBook.Close(false); //закрыть не сохраняя
             ObjWorkExcel.Quit(); // выйти из экселя
             GC.Collect(); // убрать за собой
+
+            // уничтожает к чертям абсолютно все процессы экселя. very opasno
+            System.Diagnostics.Process[] localByName = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process pr in localByName)
+                if (pr.ProcessName == "EXCEL") pr.Kill();
+
 
             // https://portal.petrocollege.ru/Lists/2014/Attachments/10/%D0%B3%D1%80%D1%83%D0%BF%D0%BF_27.09.xlsx
         }
